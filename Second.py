dd78852efa3aad7e9d19115b2187f5dc41f5a9f8
@tamaghno <tamaghnog@gmail.com>
2025-01-25 18:18:41 +0530

    Add string manipulation examples in Second.py and First.py

---
+++ Second.py
@@ -0,0 +1,10 @@
+s = "kAUSHIK"
+
+s = "K" + s[1:]
+
+print(s)
+
+
+#here what we have done -->Here, s[1:] means "take all characters in the string s starting from index 1 to the end." For "Hello", this results in "ello".
+
+#Concatenate "h" with "ello" using the + operator to form the new string "hello".
---
+++ string basics/First.py
@@ -0,0 +1,7 @@
+s1 = 'My'
+
+s2 = "name"
+
+s3 = '''is Tamaghno'''
+     
+print(s1+" "+s2+" "+s3)
