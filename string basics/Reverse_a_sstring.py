2ed56db053fef991dc1fd6961c1912b605b230a0
@tamaghno <tamaghnog@gmail.com>
2025-01-25 19:02:50 +0530

    Add recursive string reversal function to Reverse_a_string.py

--- string basics/Reverse_a_sstring.py
+++ string basics/Reverse_a_sstring.py
@@ -1,1 +1,10 @@
 # Method using recursion
+def reversing_using_recursion(s):
+    if len(s) == 0:
+        return s
+    return s[-1] + reversing_using_recursion(s[:-1])
+
+
+striing = input("Enter a string to reverse : ")
+print("Reversed using recursion : ",reversing_using_recursion(striing))
+        
