60420a25aea88d7dd3d344e149a13d013f499d8c
@tamaghno <tamaghnog@gmail.com>
2025-01-30 20:54:02 +0530

    Enhance Dog class with detailed comments on __init__ method and self usage; add tempCodeRunnerFile.py

--- OOP in Py/classess and objects/Dog.class.py
+++ OOP in Py/classess and objects/Dog.class.py
@@ -1,6 +1,6 @@
 class Dog:
     
-    def __init__(self, name, breed):
+    def __init__(self, name, breed):   #_init_ is a constructor having 2 attributes name and breed
         self.Name = name
         self.Breed = breed
     
@@ -8,7 +8,7 @@ class Dog:
     def bark(self):
         print("Woof woof")
         
-dog1 = Dog("Bruce", "Scottish Terrier")
+dog1 = Dog("Bruce", "Scottish Terrier")   #--->The __init__ method is automatically called when creating dog1
 dog1.bark()
 print(dog1.Name)
 print(dog1.Breed)
@@ -17,3 +17,16 @@ dog2 = Dog("Freya", "Greyhound")
 dog2.bark()
 print(dog2.Name)
 print(dog2.Breed)
+
+
+
+'''
+---> self is a reference to the instance of the class.
+---> It allows access to instance variables and instance methods.
+---> Every time an object is created, self represents that specific object.
+
+
+
+---> A class is like a blueprint, and an instance is a specific object built from that blueprint.
+---> Each instance has its own set of data (attributes) and can use the methods defined in the class.
+'''
